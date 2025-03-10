064741036b35c432bd3cab9df35ab6631cd4b7ea
Marco Facecchia <facecchiamarco@icloud.com>
2025-03-10 19:26:23 +0100

    feat: enable custom API items count and offset

--- src/utils/SongsUtil.py
+++ src/utils/SongsUtil.py
@@ -9,13 +9,18 @@ from models.Song import Song
 
 class SongsUtil:
     @staticmethod
-    def fetch_songs() -> List[Song]:
+    def fetch_songs(songs_count: int, page: int) -> List[Song]:
+        if songs_count <= 0 or page <= 0:
+            raise ValueError("Songs count or page cannot be lower than 1.")
+        if songs_count > 100:
+            raise ValueError(
+                "Songs count cannot be higher than 100 due to API constraints."
+            )
         playlist_id = "6yPiKpy7evrwvZodByKvM9"
-        offset = 100
+        offset = page
         host = "spotify23.p.rapidapi.com"
         api_key = os.getenv("RAPIDAPI-SPOTIFY-KEY")
-        # Max value allowed by this API
-        limit = 100
+        limit = songs_count
         res = requests.get(
             str.format(
                 "https://spotify23.p.rapidapi.com/playlist_tracks/?id={playlist_id}&offset={offset}&limit={limit}",
