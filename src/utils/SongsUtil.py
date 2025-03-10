0d5df5ba890b7bf480aa95a08f5bd931d5738d6f
Marco Facecchia <facecchiamarco@icloud.com>
2025-03-10 19:08:26 +0100

    feat: add api data feth implementation

--- src/utils/SongsUtil.py
+++ src/utils/SongsUtil.py
@@ -1,18 +1,53 @@
+import os
 from typing import List
+
+import requests
+from requests.exceptions import HTTPError
 
 from models.Song import Song
 
 
 class SongsUtil:
-    # TODO: Make API call here
     @staticmethod
     def fetch_songs() -> List[Song]:
+        playlist_id = "6yPiKpy7evrwvZodByKvM9"
+        offset = 100
+        host = "spotify23.p.rapidapi.com"
+        api_key = os.getenv("RAPIDAPI-SPOTIFY-KEY")
+        # Max value allowed by this API
+        limit = 100
+        res = requests.get(
+            str.format(
+                "https://spotify23.p.rapidapi.com/playlist_tracks/?id={playlist_id}&offset={offset}&limit={limit}",
+                playlist_id=playlist_id,
+                offset=offset,
+                limit=limit,
+            ),
+            headers={
+                "content-type": "application/json",
+                "x-rapidapi-host": host,
+                "x-rapidapi-key": api_key,
+            },
+        )
+        if not res.ok:
+            raise HTTPError(
+                str.format(
+                    "Server responses with a {status} HTTP status code",
+                    status=res.status_code,
+                )
+            )
+        items = res.json()["items"]
+
         songs: List[Song] = []
-        sample_song: Song = Song()
-        sample_song.set_title("Reflections Laughing")
-        sample_song.set_artist_name("The Weeknd")
-        sample_song.set_is_explicit(True)
-        sample_song.set_duration(300)
-        songs.append(sample_song)
+
+        for entry in items:
+            track = entry["track"]
+
+            song = Song()
+            song.set_title(track["name"])
+            song.set_is_explicit(track["explicit"])
+            song.set_artist_name(track["artists"][0]["name"])
+            song.set_duration(track["duration_ms"] / 1000)
+            songs.append(song)
 
         return songs
