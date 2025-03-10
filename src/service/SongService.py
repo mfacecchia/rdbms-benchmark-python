c8f5082fae890853f160c70180157462e9e7a681
Marco Facecchia <facecchiamarco@icloud.com>
2025-03-10 09:41:39 +0100

    refactor: song service class to be concrete

--- src/service/SongService.py
+++ src/service/SongService.py
@@ -1,19 +1,26 @@
-from abc import abstractmethod
-from typing import List
+from typing import Any, List
 
+from databases.DatabaseConnection import DatabaseConnection
 from models.Song import Song
+from repository.SongRepository import SongRepository
 
 
 class SongService:
+    _conn: DatabaseConnection[Any]
+    _song_repository: SongRepository
 
-    @abstractmethod
+    def __init__(self, conn: DatabaseConnection[Any], song_repository: SongRepository):
+        self._conn = conn
+        self._song_repository = song_repository
+
     def find_all(self) -> List[Song]:
-        pass
+        return self._song_repository.find_all(self._conn)
 
-    @abstractmethod
     def delete_all(self) -> None:
-        pass
+        self._song_repository.delete_all(self._conn)
 
-    @abstractmethod
     def save(self, song: Song) -> None:
-        pass
+        self._song_repository.save(self._conn, song)
+
+    def get_conn(self) -> DatabaseConnection[Any]:
+        return self._conn
