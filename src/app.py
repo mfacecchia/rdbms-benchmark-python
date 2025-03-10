1f318f2d4af465df5d1df8bbba6ec4ef6f872bb2
Marco Facecchia <facecchiamarco@icloud.com>
2025-03-10 19:44:26 +0100

    feat: add main app script

--- src/app.py
+++ src/app.py
@@ -1,5 +1,29 @@
+from pprint import pprint
+from typing import List
+
+from benchmark.Benchmark import Benchmark
+from databases.MySql import MySql
+from databases.PostgreSql import PostgreSql
+from repository.MySqlSongRepositoryImpl import (
+    MySqlSongRepositoryImpl as MySqlSongRepository,
+)
+from repository.PostgreSqlSongRepositoryImpl import (
+    PostgreSqlSongRepositoryImpl as PostgreSqlSongRepository,
+)
+from service.SongService import SongService
+
+
 def main():
-    print("Hello, world!")
+    services: List[SongService] = [
+        SongService(MySql(), MySqlSongRepository()),
+        SongService(PostgreSql(), PostgreSqlSongRepository()),
+    ]
+    benchmark = Benchmark(services)
+    benchmark.begin_benchmark()
+
+    print("----Results----")
+    pprint(benchmark.get_results(), indent=4)
+    print("---------------")
 
 
 if __name__ == "__main__":
